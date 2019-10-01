fc0d08fa5719ef2e2bf3c14afcaf0b2d8edffc91
Raphael Mazelier <raph@futomaki.net>
2019-10-01 14:21:57 +0200

    With the files :)

---
+++ check/check.go
@@ -0,0 +1,112 @@
+package check
+
+import (
+	"errors"
+	log "github.com/Sirupsen/logrus"
+	"github.com/prometheus/client_golang/prometheus"
+	"time"
+)
+
+type Check struct {
+	Name     string
+	Interval int
+	Target   string
+	Timeout  int
+	Type     string
+	Run      func(Check, chan CheckResponse)
+}
+
+type Checks struct {
+	Check []Check
+}
+
+type CheckResponse struct {
+	IsUp     int
+	Status   string
+	Duration time.Duration
+}
+
+var (
+	isupMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "checker_up",
+			Help: "Check status, 1 : up,  0 : down",
+		},
+		[]string{"check"},
+	)
+	durationMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "checker_duration",
+			Help: "Check last duration time",
+		},
+		[]string{"check"},
+	)
+	successMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "checker_success",
+			Help: "Number of succeeded checks",
+		},
+		[]string{"check"},
+	)
+	failedMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "checker_failed",
+			Help: "Number of failed checks",
+		},
+		[]string{"check"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(isupMetric)
+	prometheus.MustRegister(durationMetric)
+	prometheus.MustRegister(successMetric)
+	prometheus.MustRegister(failedMetric)
+}
+
+func (c Check) Validate() error {
+	if c.Name == "" {
+		return errors.New("name of the probe should be defined")
+	}
+	if c.Interval == 0 {
+		return errors.New("interval of the probe should be defined")
+	}
+	if c.Target == "" {
+		return errors.New("target of the probe should be defined")
+	}
+	if c.Timeout == 0 {
+		return errors.New("timeout of the probe should be defined")
+	}
+	if c.Type == "" {
+		return errors.New("type of the probe should be defined")
+	}
+	if c.Type != "http" && c.Type != "tcp" {
+		return errors.New("type of the probe is uncorrect")
+	}
+	return nil
+}
+
+func (c Check) Start() {
+	log.Printf("check thread %s started\n", c.Name)
+	ticker := time.NewTicker(time.Duration(c.Interval) * time.Millisecond)
+
+	for range ticker.C {
+		cr := make(chan CheckResponse, 1)
+		go c.Run(c, cr)
+
+		select {
+		case r := <-cr:
+			isupMetric.WithLabelValues(c.Name).Set(float64(r.IsUp))
+			successMetric.WithLabelValues(c.Name).Inc()
+			durationMetric.WithLabelValues(c.Name).Set(float64(r.Duration))
+			log.Printf("check %s %s %s %v\n", c.Name, c.Target, r.Status, r.Duration)
+		case <-time.After(time.Duration(c.Timeout) * time.Millisecond):
+			isupMetric.WithLabelValues(c.Name).Set(0)
+			failedMetric.WithLabelValues(c.Name).Inc()
+			durationMetric.WithLabelValues(c.Name).Set(float64(c.Timeout))
+			log.Printf("check %s %s timeout\n", c.Name, c.Target)
+		}
+	}
+
+	log.Printf("check thread %s ended\n", c.Name)
+}
---
+++ check/check_http/check_http.go
@@ -0,0 +1,46 @@
+package check_http
+
+import (
+	"github.com/ut0mt8/goChecker/check"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func Run(c check.Check, cr chan check.CheckResponse) {
+	var isUp int
+	var status string
+	var duration time.Duration
+
+	start := time.Now().UnixNano()
+	client := http.Client{Timeout: time.Duration(c.Timeout) * time.Millisecond}
+
+	resp, err := client.Get(c.Target)
+	if err != nil {
+		isUp = 0
+		status = "connection failed"
+		duration = 0
+	} else {
+		defer resp.Body.Close()
+		_, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			isUp = 0
+			status = "body read failed"
+			duration = 0
+		} else if resp.StatusCode >= 200 && resp.StatusCode <= 399 {
+			isUp = 1
+			status = resp.Status
+			duration = time.Duration(time.Now().UnixNano() - start)
+		} else {
+			isUp = 0
+			status = resp.Status
+			duration = time.Duration(time.Now().UnixNano() - start)
+		}
+	}
+
+	cr <- check.CheckResponse{
+		IsUp:     isUp,
+		Status:   status,
+		Duration: duration,
+	}
+}
